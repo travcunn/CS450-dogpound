f3ed4eb7275a209ee9cfe68a2169debc6244dbe3
Chris <snarbin@gmail.com>
2014-11-06 20:00:51 -0500

    Friend relationships between users. Friends table created and a few sample relationships created.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
 from app import db
 
+friends = db.Table('friends',
+    db.Column('friender_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('friendee_id', db.Integer, db.ForeignKey('user.id')))  
+
 # user class for database model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,11 +13,21 @@ class User(db.Model):
     password = db.Column(db.String(20), index=True)
 	#  adds relationship between User and Barks so a user's barks can be displayed
     barks = db.relationship('Bark', backref='author', lazy='dynamic')
+    friendList = db.relationship('User', 
+    							secondary=friends,
+    							primaryjoin=(friends.c.friender_id == id),
+    							secondaryjoin=(friends.c.friendee_id == id),
+    							backref=db.backref('friends', lazy='dynamic'),
+    							lazy='dynamic')
+    
+    def friends_barks(self):
+    	return Bark.query.join(friends, 
+    		(friends.c.friendee_id == Bark.user_id)).filter(friends.c.friender_id == self.id).order_by(Bark.timestamp.desc())
     
 	#  return the user's first name
     def __repr__(self):
         return '<User %r>' % (self.firstName)
-
+      
         
 #  Bark class for collection of barks in the feed 
 class Bark(db.Model):
