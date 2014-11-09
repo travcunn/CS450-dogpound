6842233bc0c0184b7dcc4d34b8cb25feb302118e
Travis Cunningham <travcunn@iupui.edu>
2014-11-09 18:31:38 -0500

    Removed the test users file since the same information is in the README. Remove SQLAlchemy migration files for now.

--- config.py
+++ config.py
@@ -4,6 +4,3 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # csrf protection
 WTF_CSRF_ENABLED = False
 SECRET_KEY = 'super-mario-brothers'
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
